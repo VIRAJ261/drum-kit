8e1718ac17d40b555a2620c640b8093c386c0e97
VIRAJ261 <virajvwankhede2610@gmail.com>
2024-07-15 11:18:59 +0530

    first commit

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,49 @@
+var x = document.getElementById("myAudio");
+function playClick(){
+  x.play();
+}
+function pauseClick(){
+    x.pause();
+}
+var y = document.getElementById("myAudio2");
+function playClick1(){
+  y.play();
+}
+function pauseClick1(){
+    y.pause();
+}
+var z = document.getElementById("myAudio3");
+function playClick2(){
+  z.play();
+}
+function pauseClick2(){
+    z.pause();
+}
+var m = document.getElementById("myAudio4");
+function playClick4(){
+  m.play();
+}
+function pauseClick4(){
+    m.pause();
+}
+var n = document.getElementById("myAudio5");
+function playClick5(){
+  n.play();
+}
+function pauseClick5(){
+    n.pause();
+}
+var o = document.getElementById("myAudio6");
+function playClick6(){
+  o.play();
+}
+function pauseClick6(){
+    o.pause();
+}
+var p = document.getElementById("myAudio7");
+function playClick7(){
+  p.play();
+}
+function pauseClick7(){
+    p.pause();
+}
